268118657424e194b1dcb1a4a7262fc2bcedc985
matokun95 <matias.jca95@gmail.com>
2022-05-17 17:19:03 -0400

    testing routes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -19,56 +19,13 @@ const NavBar = (darkImages, setDarkImages) => {
     }
 
     return (
-        // <nav className='top-nav'>
-        //     <div className="logo">
-        //         <NavLink to='/portfolio-react/'><img src={Logo} alt="" /></NavLink>
-        //     </div>
-        //     <ul>
-        //         <li>
-        //             <NavLink to='/portfolio-react/'>
-        //                 <FontAwesomeIcon icon={faHome} />
-        //             </NavLink>
-        //             <div className="pointer-container">
-        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
-        //             </div>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/portfolio-react/portfolio'>Portafolio</NavLink>
-        //             <div className="pointer-container">
-        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
-        //             </div>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/portfolio-react/skills'>Habilidades</NavLink>
-        //             <div className="pointer-container">
-        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
-        //             </div>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/portfolio-react/about'>Sobre mí</NavLink>
-        //             <div className="pointer-container">
-        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
-        //             </div>
-        //         </li>
-        //     </ul>
-        //     <div className="theme-button">
-        //         {
-        //             darkMode === false
-        //                 ? <FontAwesomeIcon className='theme-icon' icon={faSun} />
-        //                 : <FontAwesomeIcon className='theme-icon' icon={faMoon} />
-        //         }
-        //         <div className="slider-container" onClick={() => changeTheme()}>
-        //             <div className={darkMode === false ? 'slider' : 'slider slider-right'}></div>
-        //         </div>
-        //     </div>
-        // </nav>
         <nav className='top-nav'>
             <div className="logo">
-                <NavLink to='/'><img src={Logo} alt="" /></NavLink>
+                <NavLink to='/portfolio-react/'><img src={Logo} alt="" /></NavLink>
             </div>
             <ul>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink to='/portfolio-react/'>
                         <FontAwesomeIcon icon={faHome} />
                     </NavLink>
                     <div className="pointer-container">
@@ -76,19 +33,19 @@ const NavBar = (darkImages, setDarkImages) => {
                     </div>
                 </li>
                 <li>
-                    <NavLink to='/portfolio'>Portafolio</NavLink>
+                    <NavLink to='/portfolio-react/portfolio'>Portafolio</NavLink>
                     <div className="pointer-container">
                         <FontAwesomeIcon className='pointer' icon={faCaretUp} />
                     </div>
                 </li>
                 <li>
-                    <NavLink to='/skills'>Habilidades</NavLink>
+                    <NavLink to='/portfolio-react/skills'>Habilidades</NavLink>
                     <div className="pointer-container">
                         <FontAwesomeIcon className='pointer' icon={faCaretUp} />
                     </div>
                 </li>
                 <li>
-                    <NavLink to='/about'>Sobre mí</NavLink>
+                    <NavLink to='/portfolio-react/about'>Sobre mí</NavLink>
                     <div className="pointer-container">
                         <FontAwesomeIcon className='pointer' icon={faCaretUp} />
                     </div>
@@ -105,6 +62,49 @@ const NavBar = (darkImages, setDarkImages) => {
                 </div>
             </div>
         </nav>
+        // <nav className='top-nav'>
+        //     <div className="logo">
+        //         <NavLink to='/'><img src={Logo} alt="" /></NavLink>
+        //     </div>
+        //     <ul>
+        //         <li>
+        //             <NavLink to='/'>
+        //                 <FontAwesomeIcon icon={faHome} />
+        //             </NavLink>
+        //             <div className="pointer-container">
+        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
+        //             </div>
+        //         </li>
+        //         <li>
+        //             <NavLink to='/portfolio'>Portafolio</NavLink>
+        //             <div className="pointer-container">
+        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
+        //             </div>
+        //         </li>
+        //         <li>
+        //             <NavLink to='/skills'>Habilidades</NavLink>
+        //             <div className="pointer-container">
+        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
+        //             </div>
+        //         </li>
+        //         <li>
+        //             <NavLink to='/about'>Sobre mí</NavLink>
+        //             <div className="pointer-container">
+        //                 <FontAwesomeIcon className='pointer' icon={faCaretUp} />
+        //             </div>
+        //         </li>
+        //     </ul>
+        //     <div className="theme-button">
+        //         {
+        //             darkMode === false
+        //                 ? <FontAwesomeIcon className='theme-icon' icon={faSun} />
+        //                 : <FontAwesomeIcon className='theme-icon' icon={faMoon} />
+        //         }
+        //         <div className="slider-container" onClick={() => changeTheme()}>
+        //             <div className={darkMode === false ? 'slider' : 'slider slider-right'}></div>
+        //         </div>
+        //     </div>
+        // </nav>
     )
 }
 
